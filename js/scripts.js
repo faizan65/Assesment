7bcef392bce9387d00709cc1c4ad2bd3cab6503f
faizan65 <faizantariq65@gmail.com>
2021-10-20 17:09:28 +0500

    Upload Project

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let contact = document.getElementById('contact');
+
+function buttontext(){
+    contact.innerHTML = 'Click Contact Button From Menu';
+  }
+
+  contact.addEventListener('click', buttontext);
